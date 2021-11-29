1e555a65b5ecaceb9fdc02513c39dee716f6f1e9
rainboy <rainboylvx@qq.com>
2021-11-29 15:17:05 +0800

    加入 inline escape

--- lib/rules_inline/escape.hpp
+++ lib/rules_inline/escape.hpp
@@ -19,20 +19,22 @@ bool escape(StateInline& state,bool silent) {
     auto pos = state.pos;
     auto end = state.posMax;
 
+    //检查 是不是 backslash 
     if(state.charCodeAt(pos) != BACKSLASH_CHAR ) return  false;
 
-    for( ++pos ; pos< end ; ){
+    ++pos ;
+    if(  pos< end ){
         auto ch = state.charCodeAt(pos);
-        if( ch < 256 && ESCAPED(state.src[pos]) ){
+        if( ch < 256 && ESCAPED(state.src[pos]) ){ //可以被escaped的char
             if( !silent )
                 state.pending += state.src[pos];
             state.pos = pos+1;
             return true;
         }
 
-        if( ch == newlinen_char ){
+        if( ch == newlinen_char ){ //如后是换行符
             if( !silent ) 
-                state.push(hardbreak_type, break_tag, 0);
+                state.push(hardbreak_type, break_tag, 0); //加入<br>
 
             //skip 下一行的开头的空格
             pos++;
